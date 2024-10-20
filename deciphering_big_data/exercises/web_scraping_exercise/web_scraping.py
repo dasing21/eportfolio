6c97d41d11045cc29edec50cc5b5c56fa4b313e1
Dalbir <58086523+dasing21@users.noreply.github.com>
2024-10-20 20:10:55 +0200

    web scraping py p1

---
+++ deciphering_big_data/exercises/web_scraping_exercise/web_scraping.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+url = 'https://en.wikipedia.org/wiki/Data_science'
+word = 'Data Scientist'
+
+def scrape_and_parse(url, word):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+text = soup.get_text().lower()
+word_count = text.count(word.lower())
+
+data = {
+'url': url,
+'word': word,
+'occurences of word': word_count
+}
+
+with open('word_count_data.json', 'w') as f:
+    json.dump(data, f, indent=4)
+
+if __name__ == '__main__':
+    scrape_and_parse(url, word)
