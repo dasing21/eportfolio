ada143f59a7ce0f8f7c77aff963c4e4365981fc2
Dalbir <58086523+dasing21@users.noreply.github.com>
2024-10-21 17:28:00 +0200

    dataCleaningExercisesMerged

--- deciphering_big_data/exercises/data_cleaning_exercises/exercise_1/examples/fomatting_example.py
+++
@@ -1,9 +0,0 @@
-example_dict = {
-    'float_number': 1324.321325493,
-    'very_large_integer': 43890923148390284,
-    'percentage': .324,
-    }
-string_to_print = "float: {float_number:.4f}\n" 
-string_to_print += "integer: {very_large_integer:,}\n" 
-string_to_print += "percentage: {percentage:.2%}"
-print(string_to_print.format(**example_dict))
