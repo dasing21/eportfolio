3a05cb2d87f951671aac08d0522e3d3d4b25a863
Dalbir <58086523+dasing21@users.noreply.github.com>
2024-10-21 17:22:26 +0200

    data cleaning exercises commit

--- deciphering_big_data/exercises/web_scraping_exercise/web_scraping.py
+++ deciphering_big_data/exercises/web_scraping_exercise/web_scraping.py
@@ -2,24 +2,29 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-url = 'https://en.wikipedia.org/wiki/Data_science'
-word = 'Data Scientist'
+url = "https://en.wikipedia.org/wiki/Data_science"
+word = "Data Scientist"
+
 
 def scrape_and_parse(url, word):
     response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
+    soup = BeautifulSoup(response.content, "html.parser")
+
 
 text = soup.get_text().lower()
 word_count = text.count(word.lower())
 
-data = {
-'url': url,
-'word': word,
-'occurences of word': word_count
-}
+data = {"url": url, "word": word, "occurences of word": word_count}
 
-with open('word_count_data.json', 'w') as f:
+with open("word_count_data.json", "w") as f:
     json.dump(data, f, indent=4)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     scrape_and_parse(url, word)
+
+# EXAMPLE OF OUTPUT
+# {
+#     "url": "https://en.wikipedia.org/wiki/Data_science",
+#     "word": "Data Scientist",
+#     "occurences of word": 13,
+# }
